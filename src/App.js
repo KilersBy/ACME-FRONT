3ad5c168b49068a50ed018a05d3d88b970b2dad1
Abed Belal <abed.belal@outlook.fr>
2022-06-11 11:45:26 +0200

    Login fonctionnel

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Product from './components/Product/Product';
 import Contact from './components/Contact/Contact';
-// import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import Error404 from './components/Error404/Error404';
-// import useToken from './useToken';
 import Login from './components/Login/Login';
 
 // Copyright ABED
